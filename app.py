686102e2fce7640ccc8eaf2b792711b87324fa21
LeticiaRompava <leticia.rompava@escola.pr.gov.br>
2024-05-17 11:08:11 -0300

    adicionando função

---
+++ app.py
@@ -0,0 +1,11 @@
+def mostra():
+    nome = "Cristiano Ronaldo"
+    nascimento = "5 de fevereiro de 1985"
+    idade = 39 
+    nacionalidade = "Português"
+    posicao = "Atacante"
+    clube_atual = "Al-Nassr"
+    print(f'o {nome} nasceu no dia {nascimento} e atualmente tem {idade} anos de idade\né {nacionalidade} e atualmente joga no {clube_atual} como {posicao}.')
+
+    
+mostra()
